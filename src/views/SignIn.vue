9d361a8dcbe08537bfdd49b9547fcc2a7b7fb521
codingkai001 <codingkai001@gmail.com>
2019-04-29 22:33:30 +0800

    Optimize sign in and sign up interfaces...

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,18 +1,20 @@
 <template>
 	<div id="sign-in">
-		<el-form class="sign-container" ref="form" :model="user" label-width="100px">
-			<el-row>
-				<el-button type="success" round>登录</el-button>
-				<el-button type="info" round v-on:click="to_sign_up">注册</el-button>
-			</el-row>
-			<el-form-item label="账号">
-				<el-input v-model="user.username"></el-input>
-			</el-form-item>
-			<el-form-item label="密码">
-				<el-input v-model="user.pwd" type="password"></el-input>
-			</el-form-item>
-			<el-button type="success" round v-on:click="sign_in">登录</el-button>
-		</el-form>
+        <transition name="el-zoom-in-top">
+            <div v-show="show" class="sign-container">
+                <div class="top-button">
+                    <el-button round id="in-button" @click="to_sign_in">登录</el-button>
+                    <el-button round id="up-button" @click="to_sign_up">注册</el-button>
+                </div>
+                <div class="sign-in-form">
+                    <el-form ref="form" :model="user">
+                        <el-input v-model="user.user_account" placeholder="用户名"></el-input>
+                        <el-input v-model="user.pwd" placeholder="密码" type="password"></el-input>
+                    </el-form>
+                </div>
+                <el-button type="info" round @click="sign_in" class="to-sign">登录</el-button>
+            </div>
+        </transition>
 	</div>
 </template>
 
@@ -21,28 +23,106 @@
 		name: 'SignIn',
 		data () {
 			return {
+                show: false,
 				user: {
-					username: '',
+                    user_account: '',
 					pwd: ''
 				}
 			}
 		},
 		methods: {
-			//跳转到注册页面
-			to_sign_up () {
-				this.$router.push({ path: '/sign_up', })
-			},
+            //跳转到登录页面
+            to_sign_in() {
+                this.$router.push({path: '/sign_in',})
+            },
+            //跳转到注册页面
+            to_sign_up() {
+                this.$router.push({path: '/sign_up',})
+            },
 			//登录验证
 			sign_in () {
-				alert(this.user.username)
-				alert(this.user.pwd)
+                alert(this.user.username);
+                alert(this.user.pwd);
+
 			}
-		}
+        },
+        mounted() {
+            //组件挂载后过渡动画载入登录注册框
+            this.show = true;
+        }
 
 	}
 </script>
 
-<style scoped>
-	@import "../assets/css/sign.css";
+<style>
+    #sign-in {
+        position: absolute;
+        background: url("../assets/img/FZULibrary.jpg") no-repeat;
+        background-attachment: fixed;
+        background-size: 100%;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
 
+    #sign-in .sign-container {
+        width: 20%;
+        height: 35%;
+        margin: 15% auto;
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+        border-radius: 20px;
+        padding: 20px;
+        background-color: rgba(255, 255, 255, 0.6);
+    }
+
+    .top-button {
+        width: 80%;
+        margin: 0 10%;
+    }
+
+    #in-button, #up-button {
+        width: 40%;
+        margin: 5%;
+        height: 46px;
+        font-size: 22px;
+        font-weight: 900;
+    }
+
+    #sign-in #in-button {
+        background-color: rgba(106, 216, 216, 0.9);
+        color: white;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+    }
+
+    #sign-in #up-button {
+        background-color: rgba(255, 255, 255, 0.7);
+        color: rgb(117, 121, 125);
+    }
+
+    .sign-in-form {
+        padding: 20px;
+    }
+
+    #sign-in .el-input .el-input__inner {
+        border-radius: 18px;
+        width: 70%;
+        margin: 20px 15%;
+        background-color: rgba(255, 255, 255, 0.7);
+        color: rgb(117, 121, 125);
+        font-weight: 600;
+        font-size: 17px;
+    }
+
+    #sign-in .to-sign {
+        width: 30%;
+        margin: 0 35%;
+        height: 40px;
+        font-size: 20px;
+        line-height: 16px;
+        font-weight: 800;
+        background-color: rgba(106, 216, 216, 0.9);
+        color: white;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+        border: 1px solid white;
+    }
 </style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,54 +1,136 @@
 <template>
-	<div id="sign-up">
-		<el-form class="sign-container" ref="form" v-bind:model="user" label-width="100px">
-			<el-row>
-				<el-button type="info" round v-on:click="to_sign_in">登录</el-button>
-				<el-button type="success" round>注册</el-button>
-			</el-row>
-			<el-form-item label="账号">
-				<el-input v-model="user.username"></el-input>
-			</el-form-item>
-			<el-form-item label="密码">
-				<el-input v-model="user.pwd1" type="password"></el-input>
-			</el-form-item>
-			<el-form-item label="确认密码">
-				<el-input v-model="user.pwd2" type="password"></el-input>
-			</el-form-item>
-			<el-button type="success" round v-on:click="sign_up">注册</el-button>
-		</el-form>
-	</div>
+    <div id="sign-up">
+        <transition name="el-zoom-in-top">
+            <div v-show="show" class="sign-container">
+                <div class="top-button">
+                    <el-button round id="in-button" @click="to_sign_in">登录</el-button>
+                    <el-button round id="up-button" @click="to_sign_up">注册</el-button>
+                </div>
+                <div class="sign-up-form">
+                    <el-form ref="form" :model="reg_stu">
+                        <el-input v-model="reg_stu.user_account" placeholder="学号"></el-input>
+                        <el-input v-model="reg_stu.username" placeholder="姓名"></el-input>
+                        <el-input v-model="reg_stu.pwd1" placeholder="密码" type="password"></el-input>
+                        <el-input v-model="reg_stu.pwd2" placeholder="确认密码" type="password"></el-input>
+                        <el-input v-model="reg_stu.user_school" placeholder="学校"></el-input>
+                    </el-form>
+                </div>
+                <el-button type="info" round @click="sign_up" class="to-sign">注册</el-button>
+            </div>
+        </transition>
+    </div>
 </template>
 
 
 <script>
-	// import '../assets/js/main.js'
 
-	export default {
-		name: 'SignUp',
-		data () {
-			return {
-				user: {
-					username: '',
-					pwd1: '',
-					pwd2: ''
-				}
-			}
-		},
-		methods: {
-			//跳转到登录页面
-			to_sign_in () {
-				this.$router.push({ path: '/sign_in', })
-			},
-			//注册验证
-			sign_up () {
-				alert(this.user.username)
-				alert(this.user.pwd1)
-			}
-		}
-	}
+    export default {
+        name: 'SignUp',
+        data() {
+            return {
+                show: false,
+                reg_stu: {
+                    user_account: '',
+                    username: '',
+                    pwd1: '',
+                    pwd2: '',
+                    user_school: '',
+                    user_role: 1
+                }
+            }
+        },
+        methods: {
+            //跳转到登录页面
+            to_sign_in() {
+                this.$router.push({path: '/sign_in',})
+            },
+            //跳转到注册页面
+            to_sign_up() {
+                this.$router.push({path: '/sign_up',})
+            },
+            //注册验证
+            sign_up() {
+                alert(this.user.username);
+                alert(this.user.pwd1);
+            }
+        },
+
+        mounted() {
+            //组件挂载后过渡动画载入登录注册框
+            this.show = true;
+        }
+    }
 </script>
 
-<style scoped>
-	@import "../assets/css/sign.css";
+<style>
+    #sign-up {
+        position: absolute;
+        background: url("../assets/img/FZULibrary.jpg") no-repeat;
+        background-attachment: fixed;
+        background-size: 100%;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
 
+    #sign-up .sign-container {
+        width: 20%;
+        height: 42%;
+        margin: 15% auto;
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+        border-radius: 20px;
+        padding: 20px;
+        background-color: rgba(255, 255, 255, 0.6);
+    }
+
+    .top-button {
+        width: 80%;
+        margin: 0 10%;
+    }
+
+    #in-button, #up-button {
+        width: 40%;
+        margin: 5%;
+        height: 46px;
+        font-size: 22px;
+        font-weight: 900;
+    }
+
+    #sign-up #in-button {
+        background-color: rgba(255, 255, 255, 0.7);
+        color: rgb(117, 121, 125);
+    }
+
+    #sign-up #up-button {
+        background-color: rgba(106, 216, 216, 0.9);
+        color: white;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+    }
+
+    .sign-up-form {
+        padding: 20px;
+    }
+
+    #sign-up .el-input .el-input__inner {
+        border-radius: 18px;
+        width: 80%;
+        margin: 5px 10%;
+        background-color: rgba(255, 255, 255, 0.7);
+        color: rgb(117, 121, 125);
+        font-weight: 600;
+        font-size: 17px;
+    }
+
+    #sign-up .to-sign {
+        width: 30%;
+        margin: 0 35%;
+        height: 40px;
+        font-size: 20px;
+        line-height: 16px;
+        font-weight: 800;
+        background-color: rgba(106, 216, 216, 0.9);
+        color: white;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+        border: 1px solid white;
+    }
 </style>
