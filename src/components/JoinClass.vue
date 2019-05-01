629a9b967d7408f304171b6255da3ef98855cab9
codingkai001 <codingkai001@gmail.com>
2019-05-01 20:13:05 +0800

    optimize join class and add class vues...

---
+++ src/components/JoinClass.vue
@@ -0,0 +1,89 @@
+<template>
+    <div id="join-class">
+        <el-form :model="class_params" ref="form">
+            <el-input placeholder="班级Token" v-model="class_params.token"></el-input>
+            <div class="extra-info">
+                <span>请填写下列信息</span>
+                <el-input placeholder="姓名" v-model="class_params.name"></el-input>
+                <el-input placeholder="博客园地址" v-model="class_params.blog_address"></el-input>
+                <el-input placeholder="Github地址" v-model="class_params.github_address"></el-input>
+                <el-input placeholder="邮箱地址" v-model="class_params.email"></el-input>
+            </div>
+            <div class="confirm-button">
+                <el-button round id="join">加入班级</el-button>
+                <el-button round id="cancel">返回</el-button>
+            </div>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'JoinClass',
+        data() {
+            return {
+                class_params: {
+                    token: '',
+                    name: '',
+                    blog_address: '',
+                    github_address: '',
+                    email: ''
+                },
+
+            }
+        },
+        methods: {},
+        components: {}
+    }
+</script>
+
+<style>
+    #join-class {
+        /*border: 1px solid red;*/
+        /*padding: 20px;*/
+        width: 80%;
+        margin: 20px auto;
+    }
+
+    #join-class .extra-info span {
+        display: block;
+        font-size: 20px;
+        font-weight: 600;
+        text-align: center;
+    }
+
+    #join-class .el-input__inner {
+        width: 60%;
+        height: 40px;
+        border-radius: 18px;
+        margin: 20px 20%;
+        background-color: rgba(255, 255, 255, 0.7);
+        color: rgb(117, 121, 125);
+        font-weight: 600;
+        font-size: 17px;
+    }
+
+    #join-class .confirm-button {
+        width: 50%;
+        /*border: 1px solid red;*/
+        margin: 20px auto;
+    }
+
+    #join-class .confirm-button .el-button {
+        font-size: 20px;
+        background-color: rgba(47, 198, 225, 0.8);
+        color: white;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+        /*border: 1px solid white;*/
+        font-weight: 800;
+        /*margin: 20px 19%;*/
+    }
+
+    #join-class .confirm-button #join {
+        float: left;
+    }
+
+    #join-class .confirm-button #cancel {
+        float: right;
+    }
+</style>
--- src/components/NewClass.vue
+++ src/components/NewClass.vue
@@ -1,34 +1,23 @@
 <template>
     <div id="new-class">
-        <el-form :model="form" label-width="80px" ref="form">
-            <el-form-item label="班级名称">
-                <el-input placeholder="请输入班级名称" v-model="form.className"></el-input>
-            </el-form-item>
-            <el-form-item label="学校名称">
-                <el-input placeholder="请输入学校名称" v-model="form.schoolName"></el-input>
-            </el-form-item>
-            <el-form-item label="学院名称">
-                <el-input placeholder="请输入学院名称" v-model="form.collegeName"></el-input>
-            </el-form-item>
-            <el-form-item label="专业名称">
-                <el-input placeholder="请输入专业名称" v-model="form.majorName"></el-input>
-            </el-form-item>
-
-            <el-form-item label="班级公告">
-                <el-input
-                    :autosize="{ minRows: 2, maxRows: 8}"
-                    placeholder="请输入班级公告内容"
-                    type="textarea"
-                    v-model="form.classBulletin">
-                </el-input>
-            </el-form-item>
-            <el-form-item>
+        <el-form :model="class_form" ref="form">
+            <el-input placeholder="班级名称" v-model="class_form.name"></el-input>
+            <el-input placeholder="学校名称" v-model="class_form.school"></el-input>
+            <el-input placeholder="学院名称" v-model="class_form.college"></el-input>
+            <el-input placeholder="专业名称" v-model="class_form.major"></el-input>
+            <el-input
+                :autosize="{ minRows: 5, maxRows: 10}"
+                placeholder="班级公告"
+                type="textarea"
+                v-model="class_form.notice">
+            </el-input>
+            <div class="tags">
                 <el-tag
                     :disable-transitions="false"
                     :key="tag"
                     @close="handleClose(tag)"
                     closable
-                    v-for="tag in form.dynamicTags">
+                    v-for="tag in class_form.tags">
                     {{tag}}
                 </el-tag>
                 <el-input
@@ -37,16 +26,16 @@
                     class="input-new-tag"
                     ref="saveTagInput"
                     size="small"
-                    v-if="form.inputVisible"
-                    v-model="form.inputValue"
+                    v-if="class_form.inputVisible"
+                    v-model="class_form.inputValue"
                 >
                 </el-input>
                 <el-button @click="showInput" class="button-new-tag" size="small" v-else>+ New Tag</el-button>
-            </el-form-item>
-            <el-form-item>
-                <el-button @click="onSubmit" round type="primary">创建班级</el-button>
-                <el-button round>取消</el-button>
-            </el-form-item>
+            </div>
+            <div class="confirm-button">
+                <el-button round id="join">创建班级</el-button>
+                <el-button round id="cancel">取消</el-button>
+            </div>
         </el-form>
     </div>
 </template>
@@ -54,66 +43,115 @@
 <script>
     export default {
         name: 'NewClass',
-        data () {
+        data() {
             return {
-                form: {
-                    className: '',
-                    schoolName: '',
-                    collegeName: '',
-                    majorName: '',
-                    classBulletin: '',
-                    dynamicTags: ['学号', '姓名', '博客园地址', 'Github地址', '邮箱'],
+                class_form: {
+                    name: '',
+                    school: '',
+                    college: '',
+                    major: '',
+                    notice: '',
+                    tags: ['姓名', '博客园地址', 'Github地址', '邮箱'],
                     inputVisible: false,
                     inputValue: ''
                 }
             }
         },
         methods: {
-            handleClose (tag) {
-                this.form.dynamicTags.splice(this.form.dynamicTags.indexOf(tag), 1)
+            handleClose(tag) {
+                this.class_form.tags.splice(this.class_form.tags.indexOf(tag), 1)
             },
 
-            showInput () {
-                // console.log();
-                this.form.inputVisible = true;
+            showInput() {
+                this.class_form.inputVisible = true;
                 this.$nextTick(_ => {
                     this.$refs.saveTagInput.$refs.input.focus()
                 })
             },
 
-            handleInputConfirm () {
-                let inputValue = this.form.inputValue
+            handleInputConfirm() {
+                let inputValue = this.class_form.inputValue;
                 if (inputValue) {
-                    this.form.dynamicTags.push(inputValue)
+                    this.class_form.tags.push(inputValue)
                 }
-                this.form.inputVisible = false;
-                this.form.inputValue = ''
+                this.class_form.inputVisible = false;
+                this.class_form.inputValue = '';
             },
-            onSubmit() {
-                alert(this.form.dynamicTags);
-            }
         },
         components: {}
     }
 </script>
 
-<style scoped>
-    /*新增style*/
-    .el-tag + .el-tag {
-        margin-left: 10px;
+<style>
+    #new-class {
+        width: 80%;
+        margin: 20px auto;
     }
 
-    .button-new-tag {
-        margin-left: 10px;
-        height: 32px;
-        line-height: 30px;
-        padding-top: 0;
-        padding-bottom: 0;
+    #new-class .el-input__inner,
+    #new-class .el-textarea__inner {
+        width: 50%;
+        height: 40px;
+        border-radius: 18px;
+        margin: 15px 25%;
+        background-color: rgba(255, 255, 255, 0.7);
+        color: rgb(117, 121, 125);
+        font-weight: 600;
+        font-size: 17px;
     }
 
-    .input-new-tag {
-        width: 90px;
-        margin-left: 10px;
-        vertical-align: bottom;
+    #new-class .tags {
+        width: 60%;
+        margin: 15px 20%;
+        border: 1px solid red;
     }
+
+    #new-class .tags .el-tag,
+    #new-class .tags .el-button {
+        font-size: 18px;
+        margin: 10px;
+        color: white;
+        border-radius: 10px;
+        border: none;
+        background-color: rgba(254, 140, 145, 0.8);
+    }
+
+    #new-class .tags .el-button {
+        font-size: 15px;
+        color: white;
+        background-color: deepskyblue;
+    }
+
+    #new-class .tags .el-input__inner {
+        background-color: rgba(255, 255, 255, 0.7);
+        color: rgb(117, 121, 125);
+        /*font-weight: 600;*/
+        font-size: 15px;
+        width: auto;
+    }
+
+    #new-class .confirm-button {
+        width: 50%;
+        /*border: 1px solid red;*/
+        margin: 20px auto;
+    }
+
+    #new-class .confirm-button .el-button {
+        font-size: 20px;
+        background-color: rgba(47, 198, 225, 0.8);
+        color: white;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+        /*border: 1px solid white;*/
+        font-weight: 800;
+        /*margin: 20px 19%;*/
+    }
+
+    #new-class .confirm-button #join {
+        float: left;
+    }
+
+    #new-class .confirm-button #cancel {
+        float: right;
+    }
+
 </style>
