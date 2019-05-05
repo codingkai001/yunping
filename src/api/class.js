31b03eb74919f941d546ebb3dd90d18a8a515525
temporaryforfzuse <temporaryforfzuse@zsxsoft.com>
2019-05-05 09:16:30 +0800

    加入班级

--- src/api/class.js
+++ src/api/class.js
@@ -3,6 +3,6 @@ import { ClassUpdateDTO } from '../models/Class'
 
 export const classSearch = (className = '', pageNum = 1, pageSize = 9) => post('/class/search', { className, pageNum, pageSize })
 export const classAdd = (className) => post('/class/add', { className })
-export const classAuth = (token) => post('/class/auth', { token })
+export const classAuth = (form) => post('/class/auth', form)
 export const classUpdate = (form = ClassUpdateDTO) => post('/class/update', form)
 export const classDetail = (classId = '', pageNum = 1, pageSize = 0) => post('/class/detail', { classId, pageNum, pageSize })
--- src/views/Class/List/JoinClass.vue
+++ src/views/Class/List/JoinClass.vue
@@ -1,14 +1,23 @@
 <template>
   <el-dialog title="加入班级" :visible.sync="show" :show-close="false" :close-on-click-modal="false">
     <div v-loading="loading">
-      <el-form>
-        <el-form-item label="班级Token">
+      <el-form :rules="rules" :model="form" ref="ruleForm" label-width="100px">
+        <el-form-item label="班级Token" prop="token">
           <el-input v-model="form.token" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="E-mail" prop="cuEmail">
+          <el-input v-model="form.cuEmail" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="博客地址" prop="cuBlog">
+          <el-input v-model="form.cuBlog" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="GitHub" props="cuGithub">
+          <el-input v-model="form.cuGithub" autocomplete="off"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="hideThis">取 消</el-button>
-        <el-button type="primary" @click="joinClass">确 定</el-button>
+        <el-button type="primary" @click="joinClass('ruleForm')">确 定</el-button>
       </div>
     </div>
   </el-dialog>
@@ -16,6 +25,8 @@
 
 <script>
 import { classAuth } from '../../../api/class'
+import UserStorage from '../../../store/user'
+import { UserRole } from '../../../models/User'
 
 export default {
   data () {
@@ -26,7 +37,31 @@ export default {
         cuEmail: '',
         cuBlog: '',
         token: ''
+      },
+    }
+  },
+  computed: {
+    rules () {
+      const ret = {
+        token: [
+          { required: true, message: '请输入班级Token', trigger: 'blur' }
+        ],
+        cuEmail: [
+          { required: true, message: '请输入Email', trigger: 'blur' }
+        ],
+        cuGithub: [
+          { required: true, message: '请输入GitHub', trigger: 'blur' }
+        ],
+        cuBlog: [
+          { required: true, message: '请输入博客地址', trigger: 'blur' }
+        ]
       }
+      if (UserStorage.state.user.userRole === UserRole.teacher) {
+        delete ret.cuEmail
+        delete ret.cuGithub
+        delete ret.cuBlog
+      }
+      return ret
     }
   },
   props: {
@@ -36,14 +71,20 @@ export default {
     hideThis () {
       this.$emit('update:show', false)
     },
-    joinClass () {
-      this.loading = true
-      classAuth(this.form).then(p => {
-        this.$emit('done')
-        this.loading = false
-      }).catch(e => {
-        console.error(e)
-        this.loading = false
+    joinClass (form) {
+      this.$refs[form].validate((valid) => {
+        if (!valid) return
+        this.loading = true
+        classAuth({
+          ...this.form,
+          authCode: UserStorage.state.user.userRole
+        }).then(p => {
+          this.$emit('done')
+          this.loading = false
+        }).catch(e => {
+          console.error(e)
+          this.loading = false
+        })
       })
     }
   }
