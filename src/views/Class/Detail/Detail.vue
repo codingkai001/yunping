d0189498b693f79881d515707424d39de0bcb733
codingkai001 <codingkai001@gmail.com>
2019-05-27 23:14:12 +0800

    作业列表优化

--- src/views/Class/Detail/Detail.vue
+++ src/views/Class/Detail/Detail.vue
@@ -12,11 +12,11 @@
           <el-tab-pane label="作业" name="homeworks">
             <el-table :data="clazz.taskVOList">
               <el-table-column label="ID" prop="taskId" width="70"></el-table-column>
-              <el-table-column label="名称" prop="taskName"></el-table-column>
-              <el-table-column label="发布地址" prop="taskUrl" min-width="200" @focus="copy"></el-table-column>
-              <el-table-column label="发布者" prop="taskCreatorName" width="100"></el-table-column>
-              <el-table-column label="创建日期" prop="taskCreateAt" width="150"></el-table-column>
-              <el-table-column label="截止日期" prop="taskOverAt" width="150"></el-table-column>
+              <el-table-column label="名称" prop="taskName" width="200"></el-table-column>
+              <el-table-column label="发布地址" prop="taskUrl" width="300"></el-table-column>
+              <el-table-column label="发布者" prop="taskCreatorName" width="150"></el-table-column>
+              <el-table-column label="创建日期" prop="taskCreateAt" width="180"></el-table-column>
+              <el-table-column label="截止日期" prop="taskOverAt" width="180"></el-table-column>
               <el-table-column label="操作">
                 <template slot-scope="scope">
                   <el-button size="mini" @click="importHomework(scope.row)">导入</el-button>
@@ -42,12 +42,13 @@
             </el-table>
           </el-tab-pane>
           <el-tab-pane label="团队" name="teams">
-            <el-table :data="teamList"></el-table>
-            <el-table-column label="团队ID" prop="teamId"></el-table-column>
-            <el-table-column label="团队名称" prop="teamName"></el-table-column>
-            <el-table-column label="团队类型" prop="teamType"></el-table-column>
-            <el-table-column label="团队人数" prop="teamSize"></el-table-column>
-            <el-table-column label="团队队长" prop="teamCaptain"></el-table-column>
+            <el-table :data="teamList">
+              <el-table-column label="团队ID" prop="teamId"></el-table-column>
+              <el-table-column label="团队名称" prop="teamName"></el-table-column>
+              <el-table-column label="团队类型" prop="teamType"></el-table-column>
+              <el-table-column label="团队人数" prop="teamSize"></el-table-column>
+              <el-table-column label="团队队长" prop="teamCaptain"></el-table-column>
+            </el-table>
           </el-tab-pane>
         </el-tabs>
       </div>
@@ -92,6 +93,7 @@ export default {
       this.loading = true
       classDetail(id, 1, 1000).then(p => {
         this.clazz = p
+        this.secondsToTime(this.clazz.taskVOList)
         //this.loading = false
       })
       teamSearch(id).then(p => {
@@ -112,6 +114,32 @@ export default {
       target.select()
       document.execCommand('copy')
       this.$message.success('复制成功')
+    },
+    secondsToTime (s) {
+      for (var i = 0;i<s.length;i++){
+        var time_start =  new Date(parseInt(s[i].taskCreateAt))
+        var time_end =  new Date(parseInt(s[i].taskOverAt))
+        //开始时间
+        var year_start = time_start.getFullYear()
+        var month_start = time_start.getMonth() + 1
+        var day_start = time_start.getDate()
+        var hour_start = time_start.getHours()
+        var minute_start = time_start.getMinutes()
+        //结束时间
+        var year_end = time_end.getFullYear()
+        var month_end = time_end.getMonth() + 1
+        var day_end = time_end.getDate()
+        var hour_end = time_end.getHours()
+        var minute_end = time_end.getMinutes()
+
+        console.log(time_start)
+        console.log(time_end)
+
+        s[i].taskCreateAt = year_start.toString() + '年' + month_start.toString() + '月' + day_start.toString() + '日' + hour_start.toString() + ':' + minute_start.toString()
+        s[i].taskOverAt = year_end.toString() + '年' + month_end.toString() + '月' + day_end.toString() + '日' + hour_end.toString() + ':' + minute_end.toString()
+
+
+      }
     }
   }
 }
